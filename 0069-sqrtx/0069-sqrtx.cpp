1b0e99056ae7ba482ea8923664c09840743537bc
Gourav kerni <137972139+Gouravkerni@users.noreply.github.com>
2026-07-02 11:25:27 +0530

    Time: 0 ms (100%), Space: 8.7 MB (13.68%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+
+        if(x<2){
+            return x;
+        }
+
+        int s = 0;
+        int e = x;
+        int mid,ans;
+
+        while(s<=e){
+            mid = s+(e-s)/2;
+
+            if(mid ==x/mid){
+                return mid;
+            }
+            else if(mid < x/mid){
+                ans = mid;
+                s = mid+1;
+            }
+            else{
+                e = mid-1;
+            }
+        }
+        return ans;
+        
+    }
+};
