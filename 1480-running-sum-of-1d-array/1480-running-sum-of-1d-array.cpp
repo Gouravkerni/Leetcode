5b87082adebdff7fbc81691856945b343fb370cc
Gourav kerni <137972139+Gouravkerni@users.noreply.github.com>
2026-09-06 18:01:37 +0530

    Time: 0 ms (100%), Space: 12.5 MB (81.32%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -4,12 +4,9 @@ public:
         
         int n = nums.size();
 
-        vector<int> ans(n);
-        ans[0] = nums[0];
-
         for(int i = 1;i<n;i++){
-          ans[i] = ans[i-1] + nums[i];
+          nums[i] += nums[i-1];
         }
-        return ans;
+        return nums;
     }
 };
